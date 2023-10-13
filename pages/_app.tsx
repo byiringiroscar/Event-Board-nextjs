752303df97bf640d974341d456bff32ae4d250e3
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-10-13 14:40:29 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,18 @@ import type { AppProps } from 'next/app'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import userStore from './store';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   useEffect(() => {
     const token = localStorage.getItem('authTokenNew');
     if (token) {
       userStore.startTokenRefreshTimer();
+    }
+    // if no token user redirect to login
+    if (!token && router.pathname !== '/login' && !userStore.new_user) {
+      router.push('/auth/login');
     }
 
 
